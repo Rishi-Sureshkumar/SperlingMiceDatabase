5c3b19d81971840bd17c2f2856ed419e541d7a71
Rishi <pza4cv@virginia.edu>
2023-05-17 11:07:37 -0400

    Initial commit

---
+++ src/test/java/com/example/sperlingmicedatabase/SperlingMiceDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sperlingmicedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SperlingMiceDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
